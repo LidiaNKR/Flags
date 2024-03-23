bec29689155cd74f24ad200ea3de5170414b645b
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2024-03-23 08:42:22 +0300

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Лидия Некрасова on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
